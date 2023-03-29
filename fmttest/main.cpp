cecc990c552d4c42164e74198d6252f3549c4b99
TheDartFrog <30552512+TheDartFrog@users.noreply.github.com>
2023-03-29 14:22:25 +0200

    shorttest2 ay caramba
    the time duddeeeeeeee ahhhhh
    
    also why tf do we not have access to documentation..

---
+++ fmttest/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <fmt/ranges.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    fmt::print("amongus sus");
+    return 0;
+}
---
+++ shorttest2/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <vector>
+#include <algorithm>
+
+//Create a function called sameCharacters(), which will accept two arguments - std::vector<char> and an std::string.
+//This function should return true if both containers (the passed vector and the passed string)
+// consist of the same characters (with the same number of occurrences per character).
+
+int main() {
+
+
+
+    fmt::print("Hello there!");
+
+    return 0;
+}
+
+bool sameCharacters(std::vector<char> charVec, std::string susString) {
+    auto areSame = false;
+
+    if(charVec.size() == susString.size())
+    {
+        if()
+        {
+            areSame = true;
+        }
+
+
+    }
+
+
+
+
+
+    return areSame;
+}
